aca2e5202badba506e17c86e030d5136f370fdb7
Shawn <shawntayws@gmail.com>
2023-11-30 10:57:06 +0800

    cleaner code

--- inventory_project/settings.py
+++ inventory_project/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # apps
     'inventory',  
     'api',  
+
+    # libraries
     'rest_framework',
 
 ]
