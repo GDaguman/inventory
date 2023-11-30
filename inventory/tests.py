898843b3f5574fa02f59f481d924eb5f64a6044a
Shawn <shawntayws@gmail.com>
2023-11-30 10:33:12 +0800

    inventory test

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+class InventoryTestCase(TestCase):
+    def test_inventory_list_page_status(self):
+        response = self.client.get(reverse('inventory_list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_inventory_detail_page_status(self):
+        response = self.client.get(reverse('inventory_detail', kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, 200)
